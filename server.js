1402f2a436e46268a5bba9cf6d9049b931d05ab4
PMthedeveloper <pmportfollio@gmail.com>
2021-03-09 14:46:14 +0530

    passportAuth

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongodb-session")(session);
+const passport = require("passport");
 
 // Database connection
 const MONGO_CONNECTION_URL = "mongodb://localhost/pizzas";
@@ -32,6 +33,8 @@ var mongoStore = new MongoDbStore({
   uri: MONGO_CONNECTION_URL,
   collection: "sessions",
 });
+
+
 // Session Config
 app.use(
   session({
@@ -43,15 +46,23 @@ app.use(
   })
 );
 
+// Passport config
+const passportInit = require('./app/config/passport');
+passportInit(passport);
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash());
 
 // Assets
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
 // Global middleware
 app.use((req,res,next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 })
 
